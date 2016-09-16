535bc2ddb116012a8670c690c7343d3907785ffd
azrebiec <andrew.zrebiec@rutgers.edu>
2016-09-16 17:20:39 -0400

    tree moves left and right after hitting the edge

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -19,5 +19,10 @@ public class AppleTree : MonoBehaviour {
 		Vector3 pos = transform.position;
 		pos.x += speed * Time.deltaTime;
 		transform.position = pos;
+		if ( pos.x < -leftAndRightEdge ) {
+			speed = Mathf.Abs(speed); // Move right
+		} else if ( pos.x > leftAndRightEdge ) {
+			speed = -Mathf.Abs(speed); // Move left
+		}
 	}
 }
