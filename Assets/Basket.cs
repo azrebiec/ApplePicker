c5d64ee71dfa3b54cebcd99ad4b40d0615a432f7
azrebiec <andrew.zrebiec@rutgers.edu>
2016-09-29 20:04:36 -0400

    update

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -16,7 +16,7 @@ public class Basket : MonoBehaviour {
 	void Update () {
 		Vector3 mousePos2D = Input.mousePosition;
 		mousePos2D.z = -Camera.main.transform.position.z;
-		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
 		Vector3 pos = this.transform.position;
 		pos.x = mousePos3D.x;
 		this.transform.position = pos;
